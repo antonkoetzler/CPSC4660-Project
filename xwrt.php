1ef56c45305d35c09f8ba3bb28e07e1d9094d831
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-04-13 22:49:55 -0600

    Showing the results on the stage 2 screen

--- xwrt.php
+++ xwrt.php
@@ -16,6 +16,8 @@
   <main>
     <p class="evaluation_description">We will be evaluating XWRT by compressing and decompressing multiple XML documents from <a href="http://pskibinski.pl/research/Wratislavia">http://pskibinski.pl/research/Wratislavia/</a>. Alike to QXT, XWRT is a compressor for XML, HTML, and in general textual files.</p>
     <p class="evaluation_description">I will be creating a script in C++ that will: take a directory to an XML document, compress said XML document, decompress said XML document, and measure the time taken to do so. The goal of this is to mock a website that is compressing, sending compressed XML, and then the end user receiving said XML to then decompress and view it on their screen.</p>
+
+    <img id="evaluation" src="img/XWRT_Results.png">
   </main>
 </body>
 
